ab3695e577b7311987ce9c6bfd4b1592f00e3b1e
Biljana Bojovic <biljana.bojovic@gmail.com>
2026-03-11 02:36:55 +0100

    nr-mac-scheduler-ns3: Increased the minimum TX opportunity

--- model/nr-mac-scheduler-ns3.cc
+++ model/nr-mac-scheduler-ns3.cc
@@ -1519,7 +1519,8 @@ NrMacSchedulerNs3::DoScheduleDlData(PointInFTPlane* spoint,
 
             for (std::size_t numLc = 0; numLc < distributedBytes.size(); numLc++)
             {
-                NS_ABORT_MSG_IF(distributedBytes.at(numLc).m_bytes <= 7, "To small TX opportunity");
+                NS_ABORT_MSG_IF(distributedBytes.at(numLc).m_bytes <= 13,
+                                "To small TX opportunity");
                 bytesPerLc.at(numLc).emplace_back(distributedBytes.at(numLc).m_lcg,
                                                   distributedBytes.at(numLc).m_lcId,
                                                   distributedBytes.at(numLc).m_bytes);
