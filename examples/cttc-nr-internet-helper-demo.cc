d7859263dc58a5980b2a792527ebd3399ee6b9aa
nipuna.dulara <pererannd.21@uom.lk>
2026-03-18 13:11:53 +0530

    nr: NrInternetHelper example demonstration Includes a new example, `cttc-nr-internet-helper-demo`, to demonstrate assigning IPv4 addresses to UEs and routing traffic from a remote host. Updates CMakeLists.txt to register the new helper and example. Changes committed: 	modified:   examples/CMakeLists.txt 	new file:   examples/cttc-nr-internet-helper-demo.cc

---
+++ examples/cttc-nr-internet-helper-demo.cc
@@ -0,0 +1,116 @@
+// Copyright (c) 2026
+// SPDX-License-Identifier: GPL-2.0-only
+
+#include "ns3/applications-module.h"
+#include "ns3/core-module.h"
+#include "ns3/flow-monitor-module.h"
+#include "ns3/internet-module.h"
+#include "ns3/mobility-module.h"
+#include "ns3/nr-module.h"
+
+using namespace ns3;
+
+NS_LOG_COMPONENT_DEFINE("CttcNrInternetHelperDemo");
+
+int
+main(int argc, char* argv[])
+{
+  uint16_t gNbNum = 1;
+  uint16_t ueNumPergNb = 2;
+  Time simTime = Seconds(1.0);
+  Time appStartTime = Seconds(0.4);
+
+  CommandLine cmd(__FILE__);
+  cmd.AddValue("gNbNum", "Number of gNBs", gNbNum);
+  cmd.AddValue("ueNumPergNb", "UEs per gNB", ueNumPergNb);
+  cmd.AddValue("simTime", "Simulation time", simTime);
+  cmd.Parse(argc, argv);
+
+  GridScenarioHelper gridScenario;
+  gridScenario.SetRows(1);
+  gridScenario.SetColumns(gNbNum);
+  gridScenario.SetHorizontalBsDistance(10.0);
+  gridScenario.SetVerticalBsDistance(10.0);
+  gridScenario.SetBsHeight(10.0);
+  gridScenario.SetUtHeight(1.5);
+  gridScenario.SetSectorization(GridScenarioHelper::SINGLE);
+  gridScenario.SetBsNumber(gNbNum);
+  gridScenario.SetUtNumber(ueNumPergNb * gNbNum);
+  gridScenario.SetScenarioLength(50.0);
+  gridScenario.SetScenarioHeight(50.0);
+  gridScenario.CreateScenario();
+
+  Ptr<NrPointToPointEpcHelper> nrEpcHelper = CreateObject<NrPointToPointEpcHelper>();
+  Ptr<IdealBeamformingHelper> idealBeamformingHelper = CreateObject<IdealBeamformingHelper>();
+  Ptr<NrHelper> nrHelper = CreateObject<NrHelper>();
+  nrHelper->SetEpcHelper(nrEpcHelper);
+  nrHelper->SetBeamformingHelper(idealBeamformingHelper);
+
+  CcBwpCreator ccBwpCreator;
+  CcBwpCreator::SimpleOperationBandConf bandConf(28e9, 100e6, 1);
+  OperationBandInfo band = ccBwpCreator.CreateOperationBandContiguousCc(bandConf);
+
+  Ptr<NrChannelHelper> channelHelper = CreateObject<NrChannelHelper>();
+  channelHelper->ConfigureFactories("UMi", "Default", "ThreeGpp");
+  channelHelper->AssignChannelsToBands({band});
+  BandwidthPartInfoPtrVector allBwps = CcBwpCreator::GetAllBwps({band});
+
+  nrHelper->SetGnbAntennaAttribute("NumRows", UintegerValue(4));
+  nrHelper->SetGnbAntennaAttribute("NumColumns", UintegerValue(8));
+  nrHelper->SetUeAntennaAttribute("NumRows", UintegerValue(2));
+  nrHelper->SetUeAntennaAttribute("NumColumns", UintegerValue(4));
+
+  NetDeviceContainer gnbNetDev = nrHelper->InstallGnbDevice(gridScenario.GetBaseStations(), allBwps);
+  NetDeviceContainer ueNetDev = nrHelper->InstallUeDevice(gridScenario.GetUserTerminals(), allBwps);
+
+  nrHelper->AttachToClosestGnb(ueNetDev, gnbNetDev);
+
+  // New helper usage starts here.
+  Ptr<NrInternetHelper> nrInternetHelper = CreateObject<NrInternetHelper>();
+  nrInternetHelper->SetEpcHelper(nrEpcHelper);
+  nrInternetHelper->SetBackhaulAttributes(DataRate("100Gb/s"), 2500, Seconds(0.0));
+  nrInternetHelper->SetupRemoteHostIpv4();
+
+  InternetStackHelper internet;
+  internet.Install(gridScenario.GetUserTerminals());
+  Ipv4InterfaceContainer ueIpIfaces = nrInternetHelper->AssignUeIpv4(ueNetDev);
+  nrInternetHelper->SetupUeIpv4DefaultRoutes(gridScenario.GetUserTerminals());
+
+  uint16_t dlPort = 1234;
+  UdpServerHelper dlPacketSink(dlPort);
+  ApplicationContainer serverApps = dlPacketSink.Install(gridScenario.GetUserTerminals());
+
+  UdpClientHelper dlClient;
+  dlClient.SetAttribute("MaxPackets", UintegerValue(0xFFFFFFFF));
+  dlClient.SetAttribute("PacketSize", UintegerValue(100));
+  dlClient.SetAttribute("Interval", TimeValue(Seconds(0.001)));
+
+  ApplicationContainer clientApps;
+  for (uint32_t i = 0; i < ueIpIfaces.GetN(); ++i)
+  {
+    dlClient.SetAttribute("RemoteAddress", AddressValue(ueIpIfaces.GetAddress(i)));
+    dlClient.SetAttribute("RemotePort", UintegerValue(dlPort));
+    clientApps.Add(dlClient.Install(nrInternetHelper->GetRemoteHost()));
+  }
+
+  serverApps.Start(appStartTime);
+  clientApps.Start(appStartTime);
+  serverApps.Stop(simTime);
+  clientApps.Stop(simTime);
+
+  FlowMonitorHelper flowmonHelper;
+  NodeContainer endpointNodes;
+  endpointNodes.Add(nrInternetHelper->GetRemoteHost());
+  endpointNodes.Add(gridScenario.GetUserTerminals());
+  Ptr<FlowMonitor> monitor = flowmonHelper.Install(endpointNodes);
+
+  Simulator::Stop(simTime);
+  Simulator::Run();
+
+  monitor->CheckForLostPackets();
+  auto stats = monitor->GetFlowStats();
+  std::cout << "Flows observed: " << stats.size() << std::endl;
+
+  Simulator::Destroy();
+  return 0;
+}
